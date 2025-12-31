5b11f94855f0d466bf857e448447a7bdc8f16f8f
Gift Christian <140011591+gcl140@users.noreply.github.com>
2025-12-31 00:06:41 -0500

    improved all

--- dashboardd/context_processors.py
+++ dashboardd/context_processors.py
@@ -1,9 +1,17 @@
 from .models import *
 from datetime import datetime
+from django.utils import timezone
 
 def context(request):
+    if not request.user.is_authenticated:
+        return {
+            'year': datetime.now().year,
+            'due_payments_count': 0,
+            'pending_maintenance_count': 0,
+        }
+
     return {
         'year': datetime.now().year,
-        'due_payments_count': Payment.objects.filter(tenant__property__owner=request.user, due_date__lt=datetime.now().date(), status__in=['failed', 'pending']).count(),
+        'due_payments_count': Payment.objects.filter(tenant__property__owner=request.user,due_date__lt=timezone.now().date(),status__in=['failed', 'pending']).count(),
         'pending_maintenance_count': MaintenanceRequest.objects.filter(property__owner=request.user,status='pending').count(),
-        }
+    }
